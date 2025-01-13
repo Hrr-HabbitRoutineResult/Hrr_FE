8fdcdd81c6c90b50993c39d7de24631030207aa2
yc3697 <youngchan9683@gmail.com>
2025-01-13 19:07:49 +0900

    Feat: Profile Frame 상단 배너 xml 구현 #4 Build: circularprogressbar Gradle 추가 #4 Chore: Profile xml 주석 추가 #4

--- app/src/main/java/com/example/hrr_android/ProfileFragment.kt
+++ app/src/main/java/com/example/hrr_android/ProfileFragment.kt
@@ -5,55 +5,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.hrr_android.databinding.FragmentProfileBinding
+import com.mikhaellopez.circularprogressbar.CircularProgressBar
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentProfileBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val circularProgressBar = binding.cpbProfileLevelGauge
+
+        // 레벨 달성률 수치
+        val requiredPoint = 100
+        val earnedPoint = 76
+
+        // 퍼센트 계산
+        val progressPercentage = (earnedPoint / requiredPoint.toFloat()) * 50
+
+        // CircularProgressBar 설정 ; 그라데이션, 시간
+        circularProgressBar.apply {
+            progressBarColorDirection =
+                CircularProgressBar.GradientDirection.RIGHT_TO_LEFT
+            setProgressWithAnimation(progressPercentage, 1000) // 1초 동안
+        }
     }
+
 }
