e4090f9c0a151f003c13f9c082bb1c02a7374685
yc3697 <youngchan9683@gmail.com>
2025-01-15 14:19:53 +0900

    Feat: 참가중인 챌린지 RecyclerView xml 구현 및 연결 #4

---
+++ app/src/main/java/com/example/hrr_android/Challenge.kt
@@ -0,0 +1,7 @@
+package com.example.hrr_android
+
+data class Challenge(
+    var title: String,
+    var coverimg: Int,
+    var isCertified: Boolean = false
+)
--- app/src/main/java/com/example/hrr_android/ChallengeFragment.kt
+++ app/src/main/java/com/example/hrr_android/ChallengeFragment.kt
@@ -5,16 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.hrr_android.databinding.FragmentChallengeBinding
 
 class ChallengeFragment : Fragment() {
     private lateinit var binding: FragmentChallengeBinding
+    private var participatingChallengeList = ArrayList<Challenge>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentChallengeBinding.inflate(inflater, container, false)
+
+        //더미 데이터
+        participatingChallengeList.apply {
+            add(Challenge("토익 800점", R.drawable.img_english_book, false))
+            add(Challenge("토익 900점", R.drawable.img_english_book, true))
+        }
+
+        //데이터 유무 판단하여 뷰 전환
+        if(participatingChallengeList.size==0){
+            binding.clProfileParticipatingChallengeContentNo.visibility = View.VISIBLE
+            binding.rvProfileParticipatingChallengeContent.visibility = View.GONE
+        }
+
+        //RecyclerView Adapter 연결
+        val challengeRVAdapter = ProfileParticipatingChallengeRVAdapter(participatingChallengeList)
+        binding.rvProfileParticipatingChallengeContent.adapter = challengeRVAdapter
+        binding.rvProfileParticipatingChallengeContent.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
 
         return binding.root
     }
