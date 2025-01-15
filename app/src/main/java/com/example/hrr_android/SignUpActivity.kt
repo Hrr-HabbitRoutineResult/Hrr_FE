9f08069afdd7698e5b599a005d99ba3de5006261
yangjiae12 <yangjiae12@gmail.com>
2025-01-15 16:28:14 +0900

    Feat: SignUpActivity에 프래그먼트 연결 기능 추가 #6

--- app/src/main/java/com/example/hrr_android/SignUpActivity.kt
+++ app/src/main/java/com/example/hrr_android/SignUpActivity.kt
@@ -1,20 +1,37 @@
 package com.example.hrr_android
 
 import android.os.Bundle
+import android.text.TextUtils.replace
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.example.hrr_android.databinding.ActivitySignUpBinding
+
 
 class SignUpActivity : AppCompatActivity() {
+
+    // 뷰 바인딩 선언
+    private lateinit var binding: ActivitySignUpBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_sign_up)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // 뷰 바인딩 초기화
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 프래그먼트 로드
+        if (savedInstanceState == null) {
+            loadFragment(InfoInputFragment())
         }
+    }
+
+    // 프래그먼트 로드 함수
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(binding.fragmentContainer.id, fragment)
+            .commit()
     }
 }
