81b88c82aed672d8cd3be8cde6bb65649c283af4
yc3697 <youngchan9683@gmail.com>
2025-01-20 19:49:30 +0900

    Feat: 로드맵 변화 준비 #15

--- app/src/main/java/com/example/hrr_android/ProfileLevelFragment.kt
+++ app/src/main/java/com/example/hrr_android/ProfileLevelFragment.kt
@@ -1,10 +1,13 @@
 package com.example.hrr_android
 
+import android.content.res.Resources
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.res.ResourcesCompat.getColor
+import androidx.core.view.setPadding
 import com.example.hrr_android.databinding.FragmentProfileLevelBinding
 
 class ProfileLevelFragment : Fragment() {
@@ -21,6 +24,20 @@ class ProfileLevelFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        //레벨 달성에 따른 뷰 전환 테스트
+        binding.ivLevelBShape.apply {
+            setImageResource(R.drawable.bg_level_map_achieved)      //배경 아이콘 변경
+        }
+        val color = resources.getColor(R.color.white, context?.theme)
+        binding.tvLevelB.setTextColor(color)        //색상 변경
+
+        binding.ivLevelSShape.apply {
+            setImageResource(R.drawable.bg_level_map_achieved_first)      //배경 아이콘 변경
+        }
+        val color2 = resources.getColor(R.color.sub_03, context?.theme)
+        binding.tvLevelS.setTextColor(color2)        //색상 변경
+
     }
 
     override fun onDestroyView() {
