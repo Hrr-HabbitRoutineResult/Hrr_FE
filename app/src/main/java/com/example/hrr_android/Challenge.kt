0ae76b525142920a82b67a1ddba645db80cb284b
yangjiae12 <yangjiae12@gmail.com>
2025-01-22 15:42:36 +0900

    Feat: 챌린지 데이터 클래스에 챌린지 타입 변수 추가 #22

--- app/src/main/java/com/example/hrr_android/Challenge.kt
+++ app/src/main/java/com/example/hrr_android/Challenge.kt
@@ -4,5 +4,6 @@ data class Challenge(
     var title: String = "",
     var coverimg: Int,
     var description: String= "",        //챌린지 한 줄 소개
-    var isCertified: Boolean = false    //인증 완료 여부
+    var isCertified: Boolean = false,    //인증 완료 여부
+    val isTypeBasic: Boolean = false    // 챌린지 타입 (true: 베이직, false: 스터디)
 )
