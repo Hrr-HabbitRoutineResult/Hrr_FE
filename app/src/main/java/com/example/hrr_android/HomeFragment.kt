9a9ee4ef5a015608b0cc180619fcc5962fdbc6cb
yangjiae12 <yangjiae12@gmail.com>
2025-01-22 16:07:08 +0900

    Feat: 챌린지 카드 데이터 없을 시 ViewPager 숨기기 로직 추가 #22

--- app/src/main/java/com/example/hrr_android/HomeFragment.kt
+++ app/src/main/java/com/example/hrr_android/HomeFragment.kt
@@ -50,11 +50,8 @@ class HomeFragment : Fragment() {
         // 홈 화면에서만 사용하는 인기 챌린지 아이콘
         binding.itemHomeChallengeSummary.ivChallengePopular.visibility = View.VISIBLE
 
-        val challengeCardList = listOf(
-            Challenge("공부합시당", R.drawable.img_study, "", true, true),
-            Challenge("달리기 하실 분", R.drawable.img_running,"", false, true),
-            Challenge("열 자가 최대라길래", R.drawable.img_cook,"", true, false),
-        )
+        // 챌린지 데이터 리스트
+        val challengeCardList = loadChallengeData() // 데이터 로드 함수 호출
 
         // Adapter 초기화
         challengeAdapter = ChallengeCardVPAdapter(challengeCardList)
@@ -62,6 +59,16 @@ class HomeFragment : Fragment() {
         // ViewPager2 연결
         binding.vpHomeChallenge.apply {
             adapter = challengeAdapter
+
+            // 데이터가 없으면 visibility를 GONE으로 설정
+            visibility = if (challengeCardList.isEmpty()) View.GONE else View.VISIBLE
+        }
+
+        // 데이터가 있을 때 include 한 레이아웃 숨기기
+        if (challengeCardList.isEmpty()) {
+            binding.itemHomeChallengeCardNew.root.visibility = View.VISIBLE
+        } else {
+            binding.itemHomeChallengeCardNew.root.visibility = View.GONE
         }
 
     }
@@ -69,5 +76,18 @@ class HomeFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    // 챌린지 데이터 로드 함수
+    private fun loadChallengeData(): List<Challenge> {
+        // 참여한 챌린지가 있을 때
+        return listOf(
+            Challenge("공부합시당", R.drawable.img_study, "", true, true),
+            Challenge("달리기 하실 분", R.drawable.img_running, "", false, true),
+            Challenge("열 자가 최대라길래", R.drawable.img_cook, "", true, false),
+        )
+
+        // 사용자가 챌린지에 참여하지 않았을 때
+        // return emptyList()
     }
 }
