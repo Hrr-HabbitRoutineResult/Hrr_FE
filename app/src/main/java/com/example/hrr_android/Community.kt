b31482fcab7afd3aa451e994d4901bcf7a031243
yangjiae12 <yangjiae12@gmail.com>
2025-01-26 21:10:47 +0900

    Chore: 게시판 데이터 클래스 변수명 변경 #30

--- app/src/main/java/com/example/hrr_android/Community.kt
+++ app/src/main/java/com/example/hrr_android/Community.kt
@@ -1,7 +1,7 @@
 package com.example.hrr_android
 
 data class Community(
-    val pin: Boolean,
-    val title: String,
-    val describe: String
+    val isPinned: Boolean, // 핀 여부
+    val title: String,     // 게시판 제목
+    val description: String // 게시판 설명
 )
