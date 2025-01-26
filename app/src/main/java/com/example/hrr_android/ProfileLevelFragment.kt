3b275e07bc85af2a83190dfabdd8ef2b4601af55
yc3697 <youngchan9683@gmail.com>
2025-01-26 13:00:17 +0900

    Feat: 레벨 달성 시 레벨 아이콘 상태 변경 구현  #28

--- app/src/main/java/com/example/hrr_android/ProfileLevelFragment.kt
+++ app/src/main/java/com/example/hrr_android/ProfileLevelFragment.kt
@@ -1,23 +1,27 @@
 package com.example.hrr_android
 
-import android.content.res.Resources
+import android.content.Context
+import android.content.res.ColorStateList
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.res.ResourcesCompat.getColor
-import androidx.core.view.setPadding
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import com.example.hrr_android.databinding.FragmentProfileLevelBinding
 
 class ProfileLevelFragment : Fragment() {
     private var _binding: FragmentProfileLevelBinding? = null
     private val binding get() = _binding!!
+    private var myPoint: Int = 150
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentProfileLevelBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -25,23 +29,100 @@ class ProfileLevelFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        //레벨 달성에 따른 뷰 전환 테스트
-        binding.ivLevelBShape.apply {
-            setImageResource(R.drawable.bg_level_map_achieved)      //배경 아이콘 변경
-        }
-        val color = resources.getColor(R.color.white, context?.theme)
-        binding.tvLevelB.setTextColor(color)        //색상 변경
-
-        binding.ivLevelSShape.apply {
-            setImageResource(R.drawable.bg_level_map_achieved_first)      //배경 아이콘 변경
-        }
-        val color2 = resources.getColor(R.color.sub_03, context?.theme)
-        binding.tvLevelS.setTextColor(color2)        //색상 변경
-
+        //레벨에 따라 아이콘 상태(텍스트, 배경)를 설정
+        initLevelIcon()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+    private fun initLevelIcon(){
+        //포인트에 따른 레벨 정의
+        val level = when {
+            myPoint < 30 -> "Normal"
+            myPoint < 80 -> "Bronze"
+            myPoint < 150 -> "Silver"
+            myPoint < 250 -> "Gold"
+            myPoint < 400 -> "Master"
+            else -> "Challenger"
+        }
+        Log.d("levelDebug", "2")
+
+        // 레벨 순서 정의
+        val levels = listOf("Normal","Bronze", "Silver", "Gold", "Master", "Challenger")
+        val currentLevelIndex = levels.indexOf(level)
+
+        //레벨에 따른 아이콘과 텍스트 매칭("일반"은 아이콘 없으므로 제외, "챌린저"는 별도 로직으로 처리)
+        val iconAndText = mapOf(
+            "Bronze" to Pair(binding.ivLevelBShape, binding.tvLevelB),
+            "Silver" to Pair(binding.ivLevelSShape, binding.tvLevelS),
+            "Gold" to Pair(binding.ivLevelGShape, binding.tvLevelG),
+            "Master" to Pair(binding.ivLevelMShape, binding.tvLevelM),
+            "Challenger" to Pair(binding.ivLevelFinalShape, binding.ivLevelFinal)
+        )
+
+        Log.d("levelDebug", "3")
+        // 달성된 레벨까지만 아이콘 상태 업데이트
+        var levelIndex: Int = 0
+        for ((levelName, pair) in iconAndText) {
+            Log.d("levelDebug", levelName)
+            val bg = pair.first
+            val inner = pair.second
+
+            levelIndex = levels.indexOf(levelName)  //브론즈~챌린저(1~5)
+
+            // 현재 레벨 전까지는 달성 상태로 변경
+            if (levelIndex < currentLevelIndex) {
+                Log.d("levelDebug", "5")
+                changeIcon(bg, inner, requireContext(), R.drawable.bg_level_map_achieved, R.color.white)
+            }else if(levelIndex == currentLevelIndex){
+                // 현재 레벨은 최초 달성 여부 판단 후 변경
+                val prefs = requireContext().getSharedPreferences("Level_first", Context.MODE_PRIVATE)
+                val isFirstAchieved = prefs.getBoolean("Level_$levelName", true)
+                Log.d("levelDebug", "6")
+
+                if(isFirstAchieved){
+                    Log.d("levelDebug", "7")
+                    //그라데이션 배경
+                    changeIcon(bg, inner, requireContext(), R.drawable.bg_level_map_achieved_first, R.color.sub_03)
+                    Log.d("levelDebug", "8")
+                    //레벨 달성 바 변경
+
+                    //다이얼로그 띄우기
+                }
+                else{
+                    changeIcon(bg, inner, requireContext(), R.drawable.bg_level_map_achieved, R.color.white)
+                }
+
+                break
+            }
+
+        }
+        Log.d("levelDebug", "4")
+
+    }
+
+    private fun changeIcon(bg: ImageView, inner: View, context: Context, backgroundRes: Int, innerColorRes: Int){
+        // 배경 색상 변경
+        bg.setImageResource(backgroundRes)
+
+        // 텍스트 색상 변경
+        when (inner) {
+            is TextView -> {
+                // 텍스트 색상 변경
+                inner.setTextColor(ContextCompat.getColor(context, innerColorRes))
+            }
+            is ImageView -> {
+                // 내부 이미지 색상 변경
+                inner.imageTintList = ColorStateList.valueOf(ContextCompat.getColor(context, R.color.sub_03))
+            }
+            else -> {
+                // 처리하지 않음
+            }
+        }
+    }
+
+
 }
