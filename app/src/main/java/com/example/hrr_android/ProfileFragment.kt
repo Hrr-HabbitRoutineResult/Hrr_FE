8ecac0e6e5f9c572d100c71718e69a7e8014840b
yc3697 <youngchan9683@gmail.com>
2025-01-31 02:46:07 +0900

    Chore: 이전 버전 코드 삭제 #38

--- app/src/main/java/com/example/hrr_android/ProfileFragment.kt
+++ app/src/main/java/com/example/hrr_android/ProfileFragment.kt
@@ -6,12 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.hrr_android.databinding.FragmentProfileBinding
-import com.google.android.material.tabs.TabLayoutMediator
-import com.mikhaellopez.circularprogressbar.CircularProgressBar
 
 class ProfileFragment : Fragment() {
     private lateinit var binding: FragmentProfileBinding            //뷰 바인딩
-    //private var tabs = arrayListOf("챌린지", "인증기록", "뱃지")        //탭 제목
     private var selectedBadges = ArrayList<Badge>()                 //대표 뱃지 리스트
     private val profileCommon = ProfileCommon()     //공통 로직 인스턴스 생성
 
@@ -22,14 +19,6 @@ class ProfileFragment : Fragment() {
         binding = FragmentProfileBinding.inflate(inflater, container, false)
 
         //ViewPager2 Adapter 연결
-//        val profileVPAdapter = ProfileVPAdapter(this)
-//        binding.vpProfile.adapter = profileVPAdapter
-//
-//        //탭 제목 설정
-//        TabLayoutMediator(binding.tlProfile, binding.vpProfile){
-//            tab, position ->tab.text = tabs[position]
-//        }.attach()
-
         profileCommon.setupViewPager(binding, requireActivity(), true)
 
         //뱃지 더미 데이터 - 테스트 시 주석 해제 or 설정
@@ -40,8 +29,6 @@ class ProfileFragment : Fragment() {
         }
 
         //팔로우 클릭 처리
-//        onFollowClicked(binding.llProfileFollower, "follower")
-//        onFollowClicked(binding.llProfileFollowing, "following")
         profileCommon.onFollowClicked(requireActivity(), binding.llProfileFollower, "follower")
         profileCommon.onFollowClicked(requireActivity(), binding.llProfileFollowing, "following")
 
@@ -51,56 +38,10 @@ class ProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-//        // 레벨 달성률 반원 게이지 바
-//        val circularProgressBar = binding.cpbProfileLevelGauge
-//
-//        // 레벨 달성률 수치
-//        val requiredPoint = 100
-//        val earnedPoint = 76
-//
-//        // 퍼센트 계산
-//        val progressPercentage = (earnedPoint / requiredPoint.toFloat()) * 50       //반원이라 백분율의 절반만 사용
-//
-//        // CircularProgressBar 설정 ; 그라데이션, 시간
-//        circularProgressBar.apply {
-//            progressBarColorDirection =
-//                CircularProgressBar.GradientDirection.RIGHT_TO_LEFT
-//            setProgressWithAnimation(progressPercentage, 1000) // 1초 동안
-//        }
         //레벨 달성률 게이지 바 구현
         profileCommon.setupCircularProgressBar(binding, 76, 100)
 
         //설정한 대표 뱃지 개수에 따라 visibility 조정
-//        when(selectedBadges.size){
-//            0 -> {}
-//            1 -> {
-//                binding.ivProfileBadge01.setImageResource(selectedBadges[0].icon)
-//                binding.tvProfileBadge01.text = selectedBadges[0].name
-//            }
-//            2 -> {
-//                binding.llProfileBadge02.visibility = View.VISIBLE
-//
-//                binding.ivProfileBadge01.setImageResource(selectedBadges[0].icon)
-//                binding.tvProfileBadge01.text = selectedBadges[0].name
-//
-//                binding.ivProfileBadge02.setImageResource(selectedBadges[1].icon)
-//                binding.tvProfileBadge02.text = selectedBadges[1].name
-//            }
-//            3 -> {
-//                binding.llProfileBadge02.visibility = View.VISIBLE
-//                binding.llProfileBadge03.visibility = View.VISIBLE
-//
-//                binding.ivProfileBadge01.setImageResource(selectedBadges[0].icon)
-//                binding.tvProfileBadge01.text = selectedBadges[0].name
-//
-//                binding.ivProfileBadge02.setImageResource(selectedBadges[1].icon)
-//                binding.tvProfileBadge02.text = selectedBadges[1].name
-//
-//                binding.ivProfileBadge03.setImageResource(selectedBadges[2].icon)
-//                binding.tvProfileBadge03.text = selectedBadges[2].name
-//            }
-//        }
-
         profileCommon.setupBadges(binding, selectedBadges)
 
 
