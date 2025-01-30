7e3aaaaa10f68537f03a6f3b5748a5af3f88b9c7
yc3697 <youngchan9683@gmail.com>
2025-01-31 02:59:53 +0900

    Fix: 프로필 화면 전환 후 재사용 시 대표 뱃지 정보가 사라져 바인딩되지 않는 현상 수정  #38

--- app/src/main/java/com/example/hrr_android/ProfileFragment.kt
+++ app/src/main/java/com/example/hrr_android/ProfileFragment.kt
@@ -21,13 +21,6 @@ class ProfileFragment : Fragment() {
         //ViewPager2 Adapter 연결
         profileCommon.setupViewPager(binding, requireActivity(), true)
 
-        //뱃지 더미 데이터 - 테스트 시 주석 해제 or 설정
-        selectedBadges.apply {
-            add(Badge("프로 챌린저", R.drawable.img_badge_challenge_01))
-            add(Badge("수준급 스터디언", R.drawable.img_badge_challenge_01))
-            add(Badge("운동 스타터", R.drawable.img_badge_challenge_01))
-        }
-
         //팔로우 클릭 처리
         profileCommon.onFollowClicked(requireActivity(), binding.llProfileFollower, "follower")
         profileCommon.onFollowClicked(requireActivity(), binding.llProfileFollowing, "following")
@@ -40,6 +33,15 @@ class ProfileFragment : Fragment() {
 
         //레벨 달성률 게이지 바 구현
         profileCommon.setupCircularProgressBar(binding, 76, 100)
+
+        //뱃지 더미 데이터 - 테스트 시 주석 해제 or 설정
+        selectedBadges.clear()
+
+        selectedBadges.apply {
+            add(Badge("프로 챌린저", R.drawable.img_badge_challenge_01))
+            add(Badge("수준급 스터디언", R.drawable.img_badge_challenge_01))
+            add(Badge("운동 스타터", R.drawable.img_badge_challenge_01))
+        }
 
         //설정한 대표 뱃지 개수에 따라 visibility 조정
         profileCommon.setupBadges(binding, selectedBadges)
