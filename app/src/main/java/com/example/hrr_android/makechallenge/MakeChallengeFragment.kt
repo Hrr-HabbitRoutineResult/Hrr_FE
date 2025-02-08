780a5511ee79a86fa592b29d0b8d6f2762c0a78c
Yerin Chun <20211103@sungshin.ac.kr>
2025-02-09 00:02:24 +0900

    Refactor: make challenge 패키지로 이동

--- app/src/main/java/com/example/hrr_android/makechallenge/MakeChallengeFragment.kt
+++ app/src/main/java/com/example/hrr_android/makechallenge/MakeChallengeFragment.kt
@@ -1,4 +1,4 @@
-package com.example.hrr_android
+package com.example.hrr_android.makechallenge
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.example.hrr_android.R
 import com.example.hrr_android.databinding.FragmentMakeChallengeBinding
 import com.example.hrr_android.databinding.LayoutMakeChallengeHeaderBinding
 
