c3e686b8515f5660f49d05024339afbf5597d4cc
yc3697 <youngchan9683@gmail.com>
2025-02-10 16:36:17 +0900

    Feat: 인증 기록 api service 함수 작성 #73

--- app/src/main/java/com/example/hrr_android/UserSevice.kt
+++ app/src/main/java/com/example/hrr_android/UserSevice.kt
@@ -7,4 +7,8 @@ interface UserService {
     // 사용자 기본 정보 조회
     @GET("/api/v1/users/me")
     suspend fun getUserInfo(): Response<ApiResponse<UserResponse>>
+
+    // 챌린지 기록 조회
+    @GET("/api/v1/users/challenges/verification/history")
+    suspend fun getChallengeHistory(): Response<ApiResponse<HistoryResponse>>
 }
