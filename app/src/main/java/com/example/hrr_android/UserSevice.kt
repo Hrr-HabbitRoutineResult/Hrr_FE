d5f9b4df3b383d03834578130cd4229635d2d4e8
yangjiae12 <yangjiae12@gmail.com>
2025-02-11 23:12:24 +0900

    Feat: 참가 중인 챌린지 API 인터페이스 정의 #75

--- app/src/main/java/com/example/hrr_android/UserSevice.kt
+++ app/src/main/java/com/example/hrr_android/UserSevice.kt
@@ -2,9 +2,17 @@ package com.example.hrr_android
 
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface UserService {
     // 사용자 기본 정보 조회
     @GET("/api/v1/users/me")
     suspend fun getUserInfo(): Response<ApiResponse<UserResponse>>
+
+    // 참가 중인 챌린지 조회
+    @GET("/api/v1/users/{userId}/challenges/ongoing")
+    suspend fun getChallengesOngoing(
+        @Path("userId") userId: Int
+    ): Response<ApiResponse<ChallengesOngoingResponse>>
+
 }
