471bc0d0802140f00548591ac11c4a61f691c3de
yangjiae12 <yangjiae12@gmail.com>
2025-02-11 23:15:01 +0900

    Feat: UserRepository에서 참가 중인 챌린지 API 요청 처리 #75

--- app/src/main/java/com/example/hrr_android/UserRepository.kt
+++ app/src/main/java/com/example/hrr_android/UserRepository.kt
@@ -1,13 +1,15 @@
 package com.example.hrr_android
 
 import android.util.Log
+import com.example.hrr_android.access.repository.AuthRepository
 import java.io.IOException
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class UserRepository @Inject constructor(
-    private val userService: UserService
+    private val userService: UserService,
+    private val authRepository: AuthRepository
 ) {
     suspend fun loadProfile(): Result<UserResponse> {
         return try {
@@ -33,4 +35,24 @@ class UserRepository @Inject constructor(
         }
     }
 
+    suspend fun getChallengesOngoing(): Result<List<ChallengesOngoing>> {
+        val userId = authRepository.getUserId()
+        return try {
+            val response = userService.getChallengesOngoing(userId)
+
+            if (response.isSuccessful) {
+                val apiResponse = response.body()
+                    ?: return Result.failure(Exception("서버 응답이 비어 있습니다."))
+                return apiResponse.success?.ongoingChallenges?.let {
+                    Result.success(it)
+                } ?: Result.failure(Exception("서버 응답이 올바르지 않습니다."))
+            } else {
+                Result.failure(Exception("서버 오류 발생: ${response.code()}"))
+            }
+        } catch (e: IOException) {
+            Result.failure(Exception("네트워크 연결에 실패했습니다. 인터넷을 확인하세요."))
+        } catch (e: Exception) {
+            Result.failure(Exception("알 수 없는 오류 발생: ${e.localizedMessage}"))
+        }
+    }
 }
