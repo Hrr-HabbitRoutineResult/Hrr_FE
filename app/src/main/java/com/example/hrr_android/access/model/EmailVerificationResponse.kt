4493041bea7cd80c413eae95ae173156709c3246
yangjiae12 <yangjiae12@gmail.com>
2025-02-15 20:55:21 +0900

    Fix: 이메일 인증코드 전송 API 응답 수정에 따른 데이터 클래스 수정 #86

--- app/src/main/java/com/example/hrr_android/access/model/EmailVerificationResponse.kt
+++ app/src/main/java/com/example/hrr_android/access/model/EmailVerificationResponse.kt
@@ -1,5 +1,9 @@
 package com.example.hrr_android.access.model
 
+import com.google.gson.annotations.SerializedName
+
 data class EmailVerificationResponse(
-    val email: String
+    @SerializedName("resultType") val resultType: String,
+    @SerializedName("error") val error: String?,  // null이면 성공
+    @SerializedName("success") val success: String? // success를 String으로 변경
 )
