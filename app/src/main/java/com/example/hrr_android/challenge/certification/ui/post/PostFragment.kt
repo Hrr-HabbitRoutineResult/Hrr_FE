d9219149c3be8ee44a0684225fce9b107b2f5724
modzivv <vvgkxm@gmail.com>
2025-02-16 05:13:35 +0900

    Feat: 인증 게시물 더보기 바텀시트 다이얼로그 구현 #56

--- app/src/main/java/com/example/hrr_android/challenge/certification/ui/post/PostFragment.kt
+++ app/src/main/java/com/example/hrr_android/challenge/certification/ui/post/PostFragment.kt
@@ -1,15 +1,19 @@
 package com.example.hrr_android.challenge.certification.ui.post
 
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.hrr_android.R
 import com.example.hrr_android.challenge.certification.ui.post.adapter.CommentAdapter
+import com.example.hrr_android.databinding.DialogBottomSheetBinding
 import com.example.hrr_android.databinding.FragmentPostBinding
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 
 class PostFragment : Fragment() {
@@ -36,6 +40,11 @@ class PostFragment : Fragment() {
         binding.layoutRecordHeader.btnRecordBack.setOnClickListener {
             handleBackPressed()
         }
+
+        // 더보기 버튼 클릭 이벤트
+        binding.layoutPostProfile.btnPostMore.setOnClickListener {
+            showBottomSheet()
+        }
     }
 
     private fun setupRecyclerView() {
@@ -45,6 +54,54 @@ class PostFragment : Fragment() {
             adapter = commentAdapter
             layoutManager = LinearLayoutManager(requireContext())
         }
+    }
+
+    private fun showBottomSheet() {
+        // 어두운 배경 뷰 생성
+        val dimView = View(requireContext()).apply {
+            setBackgroundColor(Color.parseColor("#80000000"))
+            layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+            )
+        }
+
+        // 프래그먼트 루트 뷰에 어두운 배경 추가
+        (binding.root as ViewGroup).addView(dimView)
+
+        val bottomSheetDialog = BottomSheetDialog(requireContext(), R.style.BottomSheetDialog)
+        val dialogBinding = DialogBottomSheetBinding.inflate(layoutInflater)
+
+        dialogBinding.apply {
+            tvBlock.apply {
+                text = "수정"
+                setTextColor(ContextCompat.getColor(requireContext(), R.color.text_primary))
+                setOnClickListener {
+                    // TODO: 수정 처리
+                    bottomSheetDialog.dismiss()
+                }
+            }
+
+            tvReport.apply {
+                text = "삭제"
+                setOnClickListener {
+                    // TODO: 삭제 처리
+                    bottomSheetDialog.dismiss()
+                }
+            }
+
+            tvCancel.setOnClickListener {
+                bottomSheetDialog.dismiss()
+            }
+        }
+
+        // 다이얼로그가 닫힐 때 배경도 함께 제거
+        bottomSheetDialog.setOnDismissListener {
+            (binding.root as ViewGroup).removeView(dimView)
+        }
+
+        bottomSheetDialog.setContentView(dialogBinding.root)
+        bottomSheetDialog.show()
     }
 
     // 뒤로가기 동작 처리
