2dc0f718f72a7b13fc7ae51d0b6fd8df5b4a43f4
yc3697 <youngchan9683@gmail.com>
2025-02-16 18:41:51 +0900

    Feat: 설정-계정 설정 전환 구현 #101

---
+++ app/src/main/java/com/example/hrr_android/SettingAccountFragment.kt
@@ -0,0 +1,19 @@
+package com.example.hrr_android
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class SettingAccountFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_setting_account, container, false)
+    }
+
+}
--- app/src/main/java/com/example/hrr_android/SettingFragment.kt
+++ app/src/main/java/com/example/hrr_android/SettingFragment.kt
@@ -19,7 +19,6 @@ import dagger.hilt.android.AndroidEntryPoint
 class SettingFragment : Fragment() {
     private var _binding: FragmentSettingBinding? = null
     private val binding get() = _binding!!
-    private val authViewModel: AuthViewModel by viewModels()
 
 
     override fun onCreateView(
@@ -29,26 +28,15 @@ class SettingFragment : Fragment() {
         // ViewBinding 초기화
         _binding = FragmentSettingBinding.inflate(inflater, container, false)
 
-        val switchCompat = binding.switchCompat
-
-        val parent = switchCompat.parent as? ViewGroup
-        parent?.removeView(switchCompat) // 기존 SwitchCompat 제거
-
-        val newSwitch = SwitchCompat(requireContext()).apply {
-            trackDrawable = ContextCompat.getDrawable(context, R.drawable.toggle_bg)
-        }
-        parent?.addView(newSwitch) // 새로운 SwitchCompat 추가
-
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // 로그아웃 처리
-        binding.tvLogout.setOnClickListener {
-            logout()
-        }
+        initClickListener()
+
+
     }
 
     override fun onDestroyView() {
@@ -56,17 +44,15 @@ class SettingFragment : Fragment() {
         _binding = null
     }
 
-    private fun logout() {
-        authViewModel.logout() // ViewModel에서 로그아웃 처리
-        moveToLoginActivity() // 로그인 화면으로 이동
-    }
+    private fun initClickListener() {
+        // 계정 설정
+        binding.llSettingAccount.setOnClickListener {
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.fl_profile_more_fragment_container, SettingAccountFragment())
+                .commit()
+        }
 
-    private fun moveToLoginActivity() {
-        val intent = Intent(requireContext(), LoginActivity::class.java)
-        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-        startActivity(intent)
-        requireActivity().finish() // 현재 액티비티 종료
-    }
 
+    }
 
 }
