9c621fd9634fa51da8910f51f8bd10d5b906fd07
yc3697 <youngchan9683@gmail.com>
2025-02-16 18:46:51 +0900

    Feat: 로그아웃 구현 #101

--- app/src/main/java/com/example/hrr_android/SettingAccountFragment.kt
+++ app/src/main/java/com/example/hrr_android/SettingAccountFragment.kt
@@ -1,19 +1,91 @@
 package com.example.hrr_android
 
+import android.app.AlertDialog
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.example.hrr_android.access.AuthViewModel
+import com.example.hrr_android.access.ui.LoginActivity
+import com.example.hrr_android.databinding.DialogLogoutBinding
+import com.example.hrr_android.databinding.FragmentSettingAccountBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SettingAccountFragment : Fragment() {
+    private var _binding: FragmentSettingAccountBinding? = null
+    private val binding get() = _binding!!
+    private val authViewModel: AuthViewModel by viewModels()
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting_account, container, false)
+    ): View {
+        _binding = FragmentSettingAccountBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initClickListener()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun initClickListener() {
+        // 로그아웃 처리
+        binding.llSettingLogout.setOnClickListener {
+            showLinkDialog()
+        }
+
+
+    }
+
+    private fun showLinkDialog() {
+        val dialogView = LayoutInflater.from(requireContext())
+            .inflate(R.layout.dialog_logout, null)
+
+        val logoutBinding = DialogLogoutBinding.bind(dialogView) // 뷰 바인딩 사용
+
+        val dialog = AlertDialog.Builder(requireContext())
+            .setView(dialogView)
+            .create()
+
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        logoutBinding.tvLogoutYes.setOnClickListener {
+            logout()
+            dialog.dismiss()
+        }
+
+        logoutBinding.tvLogoutNo.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        dialog.show()
+    }
+
+
+    private fun logout() {
+        authViewModel.logout() // ViewModel에서 로그아웃 처리
+        moveToLoginActivity() // 로그인 화면으로 이동
+    }
+
+    private fun moveToLoginActivity() {
+        val intent = Intent(requireContext(), LoginActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intent)
+        requireActivity().finish() // 현재 액티비티 종료
+    }
 }
