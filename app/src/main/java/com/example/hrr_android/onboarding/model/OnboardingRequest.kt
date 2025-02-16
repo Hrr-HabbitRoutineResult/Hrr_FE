ed4bcf531e9664a1b03846676f7dec51584c665b
yangjiae12 <yangjiae12@gmail.com>
2025-02-17 00:53:22 +0900

    Feat: 사용자 맞춤 추천 API 데이터 모델 수정 #92

--- app/src/main/java/com/example/hrr_android/onboarding/model/OnboardingRequest.kt
+++ app/src/main/java/com/example/hrr_android/onboarding/model/OnboardingRequest.kt
@@ -3,5 +3,5 @@ package com.example.hrr_android.onboarding.model
 import com.google.gson.annotations.SerializedName
 
 data class OnboardingRequest(
-    @SerializedName("type") val type: String
+    @SerializedName("category") val category: String
 )
