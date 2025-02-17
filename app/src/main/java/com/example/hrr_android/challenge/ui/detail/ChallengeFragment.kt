c2ae4777499e76d3ae195174916cd11cf3beb394
yangjiae12 <yangjiae12@gmail.com>
2025-02-18 00:03:19 +0900

    Feat: 챌린지 프로필에서 챌린지 id 수신 #103

--- app/src/main/java/com/example/hrr_android/challenge/ui/detail/ChallengeFragment.kt
+++ app/src/main/java/com/example/hrr_android/challenge/ui/detail/ChallengeFragment.kt
@@ -1,6 +1,7 @@
 package com.example.hrr_android.challenge.ui.detail
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -52,6 +53,12 @@ class ChallengeFragment : Fragment(), ChallengeDialogInterface {
         val showDialog = arguments?.getBoolean("showCreateDialog", false) ?: false
         if (showDialog) {
             showCreateDialog()
+        }
+
+        val challengeId = arguments?.getInt("challenge_id", -1)
+
+        if (challengeId != -1) {
+            Log.d("ChallengeId", "Received challenge ID: $challengeId")
         }
 
     }
