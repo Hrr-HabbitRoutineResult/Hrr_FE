781a149ddefd6bf454288b619120fa304044c499
yangjiae12 <yangjiae12@gmail.com>
2025-02-20 00:24:56 +0900

    Feat: 비밀번호 재설정 스낵바 수정 및 삭제 #115

--- app/src/main/java/com/example/hrr_android/access/ui/fragment/PasswordResetFragment.kt
+++ app/src/main/java/com/example/hrr_android/access/ui/fragment/PasswordResetFragment.kt
@@ -83,10 +83,9 @@ class PasswordResetFragment : Fragment() {
 
         authViewModel.passwordNewResult.observe(viewLifecycleOwner) { result ->
             result.onSuccess {
-                ValidUtils.showSnackbar(requireView(), "비밀번호가 성공적으로 변경되었습니다.", binding.lineResetSecond)
                 loadNextFragment(PasswordResetCompleteFragment()) // 성공 시 다음 화면으로 이동
             }.onFailure {
-                ValidUtils.showSnackbar(requireView(), "비밀번호 변경에 실패했습니다. 다시 시도해주세요.", binding.lineResetSecond)
+                ValidUtils.showSnackbar(requireView(), "비밀번호 변경에 실패했습니다.", binding.lineResetSecond)
             }
         }
     }
