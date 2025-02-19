19830ab19164ee8ceaed70aa31d5f7c3540a32b7
yc3697 <youngchan9683@gmail.com>
2025-02-19 13:48:08 +0900

    Chore: 사용자 정보 수정 api의 요청 형식 수정 #80

--- app/src/main/java/com/example/hrr_android/ProfileUpdateRequest.kt
+++ app/src/main/java/com/example/hrr_android/ProfileUpdateRequest.kt
@@ -3,7 +3,7 @@ package com.example.hrr_android
 import com.google.gson.annotations.SerializedName
 
 data class ProfileUpdateRequest(
-    @SerializedName("name") val name: String,
+    @SerializedName("nickname") val name: String,
     @SerializedName("profilePhoto") val profilePhoto: String,
     @SerializedName("badges") val badges: List<Int?> // `null` 허용
 )
