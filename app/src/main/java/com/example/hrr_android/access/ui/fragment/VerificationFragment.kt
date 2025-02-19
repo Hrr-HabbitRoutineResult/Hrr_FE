b53dcca94ec78887d9df79ad25616df3bf094846
yangjiae12 <yangjiae12@gmail.com>
2025-02-20 00:12:21 +0900

    Feat: UI에서 임시 비밀번호 발급 API 호출 #115

--- app/src/main/java/com/example/hrr_android/access/ui/fragment/VerificationFragment.kt
+++ app/src/main/java/com/example/hrr_android/access/ui/fragment/VerificationFragment.kt
@@ -1,16 +1,17 @@
 package com.example.hrr_android.access.ui.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.activityViewModels
 import com.example.hrr_android.R
 import com.example.hrr_android.access.AuthViewModel
 import com.example.hrr_android.access.ValidUtils
+import com.example.hrr_android.access.ui.LoginActivity
 import com.example.hrr_android.databinding.FragmentVerificationBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -24,8 +25,7 @@ class VerificationFragment : Fragment() {
 
     // 유효성 상태 변수 선언
     private var isEmailValid = false
-    private var isEmailSent = false
-    private var isVerificationValid = false
+    private var isPasswordResetValid = false
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -41,31 +41,28 @@ class VerificationFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         initializeViews()
         setupEmailValidation()
-        setupVerificationProcess()
+        setupPasswordResetProcess()
         setupNextButton()
         updateNextButtonState() // 초기 상태 설정
-        // setupEnterKeyListener()
 
-        // 이메일 인증 API 호출 결과를 관찰하는 코드
-        authViewModel.isVerified.observe(viewLifecycleOwner) { isVerified ->
-            when (isVerified) {
-                true -> {
-                    ValidUtils.hideKeyboard(requireContext(), requireView())
-                    ValidUtils.showSnackbar(requireView(), "인증 코드가 전송 되었습니다.", binding.lineVerification)
-                    validateAndProceed() // 상태 업데이트
-                }
-                false -> {
-                    ValidUtils.hideKeyboard(requireContext(), requireView())
-                    ValidUtils.showSnackbar(requireView(), "인증 코드를 전송하지 못했습니다.", binding.lineVerification)
-                }
-                else -> {} // null이면 아무것도 안 함 (초기 상태)
+        // 비밀번호 재설정 API 호출 결과를 관찰하는 코드
+        authViewModel.passwordResetResult.observe(viewLifecycleOwner) { result ->
+            result.onSuccess {
+                ValidUtils.hideKeyboard(requireContext(), requireView())
+                ValidUtils.showSnackbar(requireView(), "임시 비밀번호가 전송되었습니다.", binding.lineVerification)
+
+                // 비밀번호 재설정 성공 시 다음 버튼 활성화
+                isPasswordResetValid = true
+                updateNextButtonState()
+            }.onFailure {
+                ValidUtils.hideKeyboard(requireContext(), requireView())
+                ValidUtils.showSnackbar(requireView(), "비밀번호를 재설정할 수 없습니다.", binding.lineVerification)
             }
         }
+
     }
 
     private fun initializeViews() {
-        binding.etVerification.isEnabled = false
-        binding.btnVerification.isEnabled = false
         binding.btnVerificationNext.isEnabled = false // 초기 상태 비활성화
     }
 
@@ -78,7 +75,6 @@ class VerificationFragment : Fragment() {
     }
 
     private fun updateEmailUI() {
-        // 이메일 유효성 검사 결과에 따라 UI 업데이트
         if (isEmailValid) {
             binding.btnVerificationSend.isEnabled = true
             binding.btnVerificationSend.background = ValidUtils.getButtonActiveBackground(requireContext())
@@ -90,107 +86,46 @@ class VerificationFragment : Fragment() {
         }
     }
 
-    private fun setupVerificationProcess() {
-        binding.btnVerificationSend.setOnClickListener { handleEmailSend() }
-        binding.btnVerification.setOnClickListener { handleVerification() }
+    private fun setupPasswordResetProcess() {
+        binding.btnVerificationSend.setOnClickListener { handlePasswordReset() }
     }
 
-    private fun handleEmailSend() {
+    private fun handlePasswordReset() {
         val email = binding.etVerificationEmail.text.toString()
         if (isEmailValid) {
-            // 네트워크 요청 전에 기존 인증 상태 초기화
-            authViewModel.setIsVerified(null)
-            // 이메일 인증 코드를 서버로 요청 (InfoInputFragment와 동일한 API 호출)
-            authViewModel.sendVerificationCode(email)
+            authViewModel.passwordReset(email)
 
-            isEmailSent = true
             binding.etVerificationEmail.isEnabled = false
             binding.etVerificationEmail.setTextColor(ValidUtils.getTextColorDefault(requireContext()))
             binding.btnVerificationSend.isEnabled = false
             binding.btnVerificationSend.background = ValidUtils.getButtonInactiveBackground(requireContext())
             binding.tvVerificationSend.setTextColor(ValidUtils.getTextColorDefault(requireContext()))
-            binding.etVerification.isEnabled = true
-            binding.btnVerification.isEnabled = true
-            binding.btnVerification.background = ValidUtils.getButtonActiveBackground(requireContext())
-            binding.tvVerification.setTextColor(ValidUtils.getTextColorError(requireContext()))
             ValidUtils.hideKeyboard(requireContext(), requireView())
-        }
-    }
-
-    private fun handleVerification() {
-        val email = binding.etVerificationEmail.text.toString()
-        val verificationCode = binding.etVerification.text.toString()
-
-        ValidUtils.hideKeyboard(requireContext(), requireView())
-
-        if (isEmailSent) {
-            // 이메일 인증 코드 확인 요청
-            authViewModel.confirmVerificationCode(email, verificationCode)
-
-            // ViewModel의 응답에 따라 UI 업데이트
-            authViewModel.verifiedUserId.observe(viewLifecycleOwner) { userId ->
-                if (userId != null) {  // ID가 존재하면 인증 성공
-                    isVerificationValid = true
-                    binding.etVerification.isEnabled = false
-                    binding.etVerification.setTextColor(ValidUtils.getTextColorDefault(requireContext()))
-
-                    binding.btnVerification.isEnabled = false
-                    binding.btnVerification.background = ValidUtils.getButtonInactiveBackground(requireContext())
-                    binding.tvVerification.setTextColor(ValidUtils.getTextColorDefault(requireContext()))
-
-                    ValidUtils.showSnackbar(requireView(), "이메일 인증이 완료되었습니다.", binding.lineVerification)
-                    // 인증 완료 시 다음 버튼 활성화
-                    updateNextButtonState()
-                } else {
-                    ValidUtils.hideKeyboard(requireContext(), requireView())
-                    ValidUtils.showSnackbar(requireView(), "올바른 인증 코드를 입력해 주세요.", binding.lineVerification)
-                }
-            }
         }
     }
 
     private fun setupNextButton() {
         binding.btnVerificationNext.setOnClickListener {
-            if (!isVerificationValid) {
-                ValidUtils.showSnackbar(requireView(), "이메일 인증을 진행해 주세요.", binding.lineVerification)
-            } else {
-                // 이메일 인증 완료 시 다른 프래그먼트로 이동
-                loadNextFragment(TemporaryPasswordFragment())
-            }
+            navigateToLoginActivity()
         }
     }
 
     private fun updateNextButtonState() {
-        // isVerificationValid가 true이면 다음 버튼 활성화
         ValidUtils.updateButtonState(
             binding.btnVerificationNext,
             binding.tvVerificationNext,
             binding.ivVerificationNext,
-            isVerificationValid
+            isPasswordResetValid
         )
     }
 
-    // 인증이 완료되었을 때 추가적인 처리가 필요하면 이 함수에서 처리 (여기서는 단순히 다음 버튼 상태 업데이트)
-    private fun validateAndProceed() {
-        updateNextButtonState()
+    private fun navigateToLoginActivity() {
+        val intent = Intent(requireContext(), LoginActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
+        }
+        startActivity(intent)
+        requireActivity().finish() // 현재 액티비티 종료
     }
-
-    private fun loadNextFragment(fragment: Fragment) {
-        requireActivity().supportFragmentManager.beginTransaction()
-            .replace(R.id.layout_password_fragment, fragment)
-            .addToBackStack(null)
-            .commit()
-    }
-
-/*
-    private fun setupEnterKeyListener() {
-        // 이메일 입력란에서 엔터 키를 눌렀을 때
-        ValidUtils.setEnterKeyListener(binding.etVerificationEmail, binding.btnVerificationSend)
-
-        // 인증코드 입력란에서 엔터 키를 눌렀을 때
-        ValidUtils.setEnterKeyListener(binding.etVerification, binding.btnVerification)
-    }
-*/
 
     override fun onDestroyView() {
         super.onDestroyView()
