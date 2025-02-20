b1f50619e93f5b1c627dd23bfc3d4b0790c1db1b
yangjiae12 <yangjiae12@gmail.com>
2025-02-20 14:59:02 +0900

    Chore: 엔드포인트에 슬래쉬 추가 #120

--- app/src/main/java/com/example/hrr_android/ChallengeService.kt
+++ app/src/main/java/com/example/hrr_android/ChallengeService.kt
@@ -5,7 +5,7 @@ import retrofit2.http.GET
 
 interface ChallengeService {
     // 오늘의 인기 챌린지 조회
-    @GET("api/v1/challenge/hotness")
+    @GET("/api/v1/challenge/hotness")
     suspend fun getChallengeHotness()
     : Response<ApiResponse<ChallengeHotnessResponse>>
 }
